f8461f65685099cc8e2b2b8c39cf760057b4bca7
zhongkun <1064598949@qq.com>
2020-02-29 15:30:21 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,13 +7,19 @@ Page({
    */
   data: {
     //轮播图数据
-     banners:[]
+     banners:[],
+     //菜单数据
+     lists:[],
+     //楼层数据
+     floors:[],
+     isTop:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
  onLoad(){
+   //请求轮播图数据
    request({
      url:'/home/swiperdata'
    }).then(res => {
@@ -21,7 +27,58 @@ Page({
        this.setData({
          banners:message
        })
+   }),
+   //请求导航栏数据
+   request({
+     url:"/home/catitems"
+   }).then(res => {
+    
+     const {message} = res.data
+     //需要判断第一个要写跳转，数据改造
+     const newData = message.map((v,i) => {
+       if(i === 0){
+         v.url ="/pages/category/index"
+       }
+       return v
+     })
+     this.setData({
+       list:newData
+     })
    })
- }
+   //请求楼层数据
+   request({
+     url: "/home/floordata"
+   }).then( res => {
+    
+     const {message} = res.data
+     this.setData({
+       floors:message
+     })
+   })
+ },
+
+ //回到顶部 
+  tohead(){
+    wx.pageScrollTo({
+      scrollTop:0,
+      duration:300
+    })
+  },
+  //监听滚动条变化
+  onPageScroll(e){
+    const { scrollTop} = e
+   let flag = this.data.isTop 
+     if(scrollTop > 100){
+        flag = true
+     } else {
+       flag = false
+     }
+    if (flag === this.data.isTop) return 
    
+    this.setData({
+      isTop:flag
+    })
+  }
+
+ 
 })
